c619c934c186268ab620cd001412e89cf5fdcab3
EnergikO <energiko71@gmail.com>
2022-04-14 14:07:29 +0300

    fixes

--- all_in_one.py
+++ all_in_one.py
@@ -10,8 +10,4 @@ open(zfc_path, 'w').write('')
 zfc = open(zfc_path, 'a', encoding="utf8")
 for filepath in filepaths:
     print(filepath[filepath.rindex('\\')+1::])
-<<<<<<< HEAD
     zfc.write(open(filepath, 'r', encoding="utf8").read() + '\n\n')
-=======
-    zfc.write(open(filepath, 'r', encoding="utf8").read() + '\n\n')
->>>>>>> 29ccdede57383eddf2af130b5f5cde12d3068444
